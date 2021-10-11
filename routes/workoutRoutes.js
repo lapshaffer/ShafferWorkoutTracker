328afae4fb38895288c1cb1ee27313f4539ec4f8
lapshaffer <lapshaffer@gmail.com>
2021-10-11 16:36:50 -0400

    Fixing home routes

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -6,11 +6,11 @@ router.get('/', function (req, res) {
   });
 
 router.get('/exercise', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+    res.sendFile(path.join(__dirname, '../public/html/exercise.html'));
   });
 
 router.get('/stats', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/stats.html'));
+    res.sendFile(path.join(__dirname, '../public/html/stats.html'));
   });
 
 module.exports = router
